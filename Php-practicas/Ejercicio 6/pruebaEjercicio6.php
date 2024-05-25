4e810cad2941f456e2ce6ad46dc64784606592d0
Augusto Moratti <augus.m0804@gmail.com>
2024-05-25 11:11:48 -0300

    Ultimatum

--- Php-practicas/Ejercicio 6/pruebaEjercicio6.php
+++ Php-practicas/Ejercicio 6/pruebaEjercicio6.php
@@ -1,4 +1,15 @@
 <?php
 include("ejercicio6.php");
 $nombre1 = "Augusto08";
-$devolucion1 = ejercicio6($nombre1);
+echo "el primer nombre es ".$nombre1."<br>";
+$devolucion = comprobar_nombre_usuario($nombre1);
+echo $devolucion;
+$nombre2 = "/Augus";
+echo "el segundo nombre es ".$nombre2."<br>";
+$devolucion = comprobar_nombre_usuario($nombre2);
+echo $devolucion;
+$nombre3 = "Ia";
+echo "el tercer nombre es ".$nombre3."<br>";
+$devolucion = comprobar_nombre_usuario($nombre3);
+echo $devolucion;
+?>
